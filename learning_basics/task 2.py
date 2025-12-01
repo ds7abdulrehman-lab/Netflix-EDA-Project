956ab8975a4adfd904bb4e474b8e92c16d715cfc
Abdul_Rehman <ds.7abdulrehman@gmail.com>
2025-12-01 23:07:39 +0100

    Add Matplotlib practice template

--- learning_basics/task 2.py
+++
@@ -1,8 +0,0 @@
-text = "Data Science"
-# Reverse the string
-# Count how many times the letter "a" appears
-reversed_text = text[::-1]
-print(reversed_text)
-
-counting_a = text.count("a")
-print(counting_a)
